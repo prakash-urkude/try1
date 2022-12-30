7b7ff86f7ecef608eabe8d7a5138a0e43f05cb5a
ShashiS07 <shashiss0708@gmail.com>
2022-12-30 12:00:36 +0530

    index.js done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const route=require("./route/route")
+const mongoose=require('mongoose')
+const app=express()
+
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://Shashi_Shekhar_Singh:Shashi0708@myproject.mb3u3za.mongodb.net/Shashi_Shekhar_Singh-DB?authSource=admin&replicaSet=atlas-lhj98j-shard-0&readPreference=primary&ssl=true",{useNewUrlParser:true})
+.then(()=>console.log("MongoDb is Connected"))
+.catch(err=>console.log(err))
+
+app.use("/",route)
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Express is running on port "+ (process.env.PORT || 3000))
+})
