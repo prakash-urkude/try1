9f0dd5852e4bc0fbf8ca78d911ce0c4b60df8591
ShashiS07 <shashiss0708@gmail.com>
2023-01-01 21:55:08 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const mongoose=require('mongoose')
 const app=express()
 
 app.use(express.json())
+mongoose.set('strictQuery', true)
 
 mongoose.connect("mongodb+srv://Shashi_Shekhar_Singh:Shashi0708@myproject.mb3u3za.mongodb.net/Shashi_Shekhar_Singh-Vaccine?authSource=admin&replicaSet=atlas-lhj98j-shard-0&readPreference=primary&ssl=true",{useNewUrlParser:true})
 .then(()=>console.log("MongoDb is Connected"))
